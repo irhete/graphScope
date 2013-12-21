ee961d9f483449c7f7f6572de2a3da83cb50cc02
irhete <irene.teinemaa@gmail.com>
2013-12-21 23:41:46 +0200

    graphScope pseudo

--- test.py
+++ test.py
@@ -1,3 +1,35 @@
-ddddd
+class Graph:
+    adjacencyString = ""
+    n = 0
+    m = 0
+    def __init__(self, adjacencyMatrix):
+        for row in adjacencyMatrix:
+            adjacencyString += row
+        n = len(adjacencyMatrix)
+        m = len(adjacencyMatrix[0])
 
-testirene
+class Segment:
+    def merge(self, graph):
+        return 0
+    
+
+def encodingCost(graph, segment):
+    return 0
+
+segments = []
+
+def graphScope(segment, segmentCost, graph):
+    unionCost = encodingCost(segment, graph)
+    graphCost = encodingCost(graph)
+    if unionCost - segmentCost < graphCost:
+        segment.merge(graph)
+        searchKL(segment)
+    else:
+        newSegment = Segment(graph)
+        segments.append(newSegment)
+        searchKL(newSegment)
+
+    
+    
+
+
